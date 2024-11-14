cbc75ba7bb9168ed2acf8ccccad142718c47522f
Gabriel Fernández López <hydetopia@gmail.com>
2024-11-14 14:29:01 +0100

    141101

--- 03Funciones/testing/probas.py
+++ 03Funciones/testing/probas.py
@@ -1,31 +1,7 @@
-def crea_lista(posiciones: int) -> list:
+def encontrar_letra_dni(numero_dni: int) -> str:
+    letras = ["T", "R",	"W", "A", "G", "M",	"Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
+    letra_dni = letras[numero_dni - ((numero_dni // 23) * 23)]
+    return letra_dni
 
-    lista = []
-    for i in range (posiciones):
-        lista.append(int(input("Escribe un número: ")))
-    print(lista)
-    return lista
-
-def invierte_lista(lista: list) -> list:
-    """Invierte el orden de los elementos de una lista dada
-
-    Args:
-        lista (list): la lista que se le da
-
-    Returns:
-        list: la lsta con los elementos invertidos
-
-    >>> invierte_lista([1, 2, 3, 4, 5])
-    [5, 4, 3, 2, 1]
-    >>> invierte_lista([])
-    []
-    >>> invierte_lista(['a', 'b', 'c'])
-    ['c', 'b', 'a']
-    """
-    return lista[::-1]
-
-lista = crea_lista(5)
-
-lista_invertida = invierte_lista(lista)
-
-print (lista_invertida)
+numero_dni = 39455246
+print(encontrar_letra_dni(numero_dni))
