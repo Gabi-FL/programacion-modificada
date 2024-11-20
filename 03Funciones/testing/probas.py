1249ff894cbdbfcccae160b26fc667511e049748
Gabriel Fernández López <hydetopia@gmail.com>
2024-11-20 12:49:01 +0100

    201101

--- 03Funciones/testing/probas.py
+++ 03Funciones/testing/probas.py
@@ -1,26 +1,6 @@
-def encontrar_letra_dni(numero_dni: int) -> str:
-    """te calcula la letra de tu dni
+lista_palabras = ["cinco", "dos", "tres", "cuatro", "catorce", "dieciseis"]
+longitud = 5
 
-    Args:
-        numero_dni (int): el dni sin letra
+lista_palargas = [palabra for palabra in lista_palabras if len(palabra) > longitud]
 
-    Returns:
-        str: la letra que te corresponde
-
-    >>> encontrar_letra_dni(12345678)
-    'Z'
-    >>> encontrar_letra_dni(12345679)
-    'S'
-    >>> encontrar_letra_dni(0)
-    'T'
-    >>> encontrar_letra_dni (-123)
-    'Número no válido'
-    """
-    letras = ["T", "R",	"W", "A", "G", "M",	"Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
-    if numero_dni < 0 or numero_dni > 99999999:
-        return "Número no válido"
-    letra_dni = letras[numero_dni % 23]
-    return letra_dni
-
-numero_dni = int(input("Escribe el número de DNI sin letra"))
-print(encontrar_letra_dni(numero_dni))
+print(lista_palargas)
