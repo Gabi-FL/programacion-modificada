8ecc16982b2b17702bb391fa2ca0082aaaf55383
Gabriel Fernández López <hydetopia@gmail.com>
2024-11-27 13:27:41 +0100

    271101

--- 03Funciones/testing/probas.py
+++ 03Funciones/testing/probas.py
@@ -1,6 +1,25 @@
-lista_palabras = ["cinco", "dos", "tres", "cuatro", "catorce", "dieciseis"]
-longitud = 5
+def mañana(hoy: str) -> str:
+    """Recibe un día de la semana y devuelve el siguiente
+        Args:
+        hoy(str): el día como cadena de caracteres
 
-lista_palargas = [palabra for palabra in lista_palabras if len(palabra) > longitud]
+    Returns:
+        str: el día siguiente al que has escrito
 
-print(lista_palargas)
+    >>> mañana("lunes")
+    'martes'
+    >>> mañana("domingo")
+    'lunes'
+    >>> mañana("hola")
+    'Valor no válido'
+    """
+    dias = ("lunes", "martes", "miércoles",
+            "jueves", "viernes", "sábado", "domingo")
+    if hoy in dias:
+        return dias[(dias.index(hoy) + 1) % 7]
+    else:
+        return "Valor no válido"
+
+
+hoy = input("escribe el nombre del día de la semana:\n")
+print(mañana(hoy))
