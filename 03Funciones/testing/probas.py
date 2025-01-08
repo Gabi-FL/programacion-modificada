deb4f8091dba0a9272e67818ab0a8c7919c64d1c
Gabriel Fernández López <hydetopia@gmail.com>
2025-01-08 12:55:10 +0100

    08012501

--- 03Funciones/testing/probas.py
+++ 03Funciones/testing/probas.py
@@ -1,8 +1,12 @@
-def comprueba_rimas(verso1: str, verso2: str) -> bool:
-    if verso1.endswith(verso2[-2][-1]):
-        return True
-    else:
-        return False
+lista_opciones = ("carne", "verdura", "pescado", "marisco")
+contador_elecciones = {"carne": 0, "verdura": 0, "pescado": 0, "marisco": 0}
+lista_elecciones = []
+respuesta = "s"
 
-
-comprueba_rimas("caca", "vaca")
+while respuesta == "s":
+    print("Estas son las opciones:")
+    for i in range(len(lista_opciones)):
+        print(i + 1, lista_opciones[i])
+    eleccion = int(input("Escoge una opción: "))
+    lista_elecciones.append(lista_opciones[eleccion - 1])
+    respuesta = input("¿Quieres añadir otro comensal? s/n: ")
