b82f1cf7f66f741a7b4affb318b0161dc2aabb4b
Gabriel Fernández López <hydetopia@gmail.com>
2025-01-29 12:52:39 +0100

    29012501

---
+++ 06POO/pruebas/NEWaplicacionBanco/Vista.py
@@ -0,0 +1,9 @@
+class Vista:
+    def menu1(self):
+        return (
+            """Selecciona una opción:\n
+            \t1. Agregar una cuenta\n
+            \t2. Mostrar las cuentas\n
+            \t3. Ingresar dinero\n
+            \t4. Retirar dinero\n
+            \t5. Salir\n""")
--- 06POO/pruebas/NEWaplicacionBanco/main.py
+++ 06POO/pruebas/NEWaplicacionBanco/main.py
@@ -1,8 +1,18 @@
 from CuentaBancaria import CuentaBancaria
 from Persona import Persona
+from Banco import Banco
+from Vista import Vista
 
-
+vista = Vista()
+b = Banco("Abanca", "Teis", "986789456")
 p1 = Persona("Manu Tenorio", 21, "12345678Z")
+opcion = int(input(vista.menu1()))
+if opcion == 1:
+    CuentaBancaria(p1, "123456789123")
+
+
+
+
 print(p1)
 c1 = CuentaBancaria(p1, "1234567890")
 print(c1)
