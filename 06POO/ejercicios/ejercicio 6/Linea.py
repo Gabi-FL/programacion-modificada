161e65069eabace39db103310b4b09690bae570e
Gabriel Fernandez López <hydetopia@gmail.com>
2025-02-03 20:01:06 +0100

    03022505

--- 06POO/ejercicios/ejercicio 6/Linea.py
+++ 06POO/ejercicios/ejercicio 6/Linea.py
@@ -1,21 +1,25 @@
 class Linea:
-    coordenadas: list
+    coordenadas: list[object]
 
-    def __init__(self, x, y):
-        self.coordenadas = [x, y]
-        if self.x > 100 or self.x < -100:
-            self.x = 0
-        if self.y > 100 or self.y < -100:
-            self.y = 0
+    def __init__(self, p1, p2):
+        if p1.x > 100 or p1.x < -100:
+            p1.x = 0
+        if p1.y > 100 or p1.y < -100:
+            p1.y = 0
+        if p2.x > 100 or p2.x < -100:
+            p2.x = 0
+        if p2.y > 100 or p2.y < -100:
+            p2.y = 0
         return self.coordenadas
 
-    def moverHorizontal(self, positivo: bool, valor: int):
+    def moverHorizontal(self, positivo: bool, valor: int)-> bool:
         if positivo:
             self.coordenadas = (self.coordenadas + valor)
         if positivo is False:
             self.coordenadas = (self.coordenadas + valor)
 
     def moverVertical(self, positivo: bool, valor: int):
+        
         if positivo:
             self.coordenadas = (self.coordenadas + valor)
         if positivo is False:
--- 06POO/ejercicios/ejercicio 6/Punto.py
+++ 06POO/ejercicios/ejercicio 6/Punto.py
@@ -1,6 +1,6 @@
 class Punto:
-    x: int
-    y: int
+    __x: int
+    __y: int
 
     def __init__(self, x, y):
         self.x = x
@@ -10,16 +10,52 @@ class Punto:
         if self.y > 100 or self.y < -100:
             self.y = 0
 
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x: int):
+        if Punto.puntoXcorrecto(x):
+            self.__x = x
+        else:
+            self.__x = x
+
+    @staticmethod
+    def puntoXcorrecto(x: int):
+        if x > 100 or x < -100:
+            return True
+        else:
+            return False
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y: int):
+        if Punto.puntoYcorrecto(y):
+            self.__y = y
+        else:
+            self.__y = y
+
+    @staticmethod
+    def puntoYcorrecto(y: int):
+        if y > 100 or y < -100:
+            return True
+        else:
+            return False
+
     def moverHorizontal(self, positivo: bool, valor: int):
         if positivo:
             self.x = (self.x + valor) % 100
-        if positivo is False:
+        else:
             self.x = (self.x + valor) % 100
 
     def moverVertical(self, positivo: bool, valor: int):
         if positivo:
             self.y = (self.y + valor) % 100
-        if positivo is False:
+        else:
             self.y = (self.y + valor) % 100
 
     def obtenerCuadrante(self):
@@ -34,3 +70,17 @@ class Punto:
 
     def __str__(self):
         return f"[{self.x}, {self.y}]"
+
+
+class Linea:
+    coordenadas: list[object]
+
+    def __init__(self, p1, p2):
+        self.p1 = [Punto.x, Punto.y]
+        self.p2 = [Punto.x, Punto.y]
+        self.coordenadas = [p1, p2]
+
+    def moverHorizontal(self, positivo: bool, valor: int)-> bool:
+
+
+    def moverVertical(self, positivo: bool, valor: int):
